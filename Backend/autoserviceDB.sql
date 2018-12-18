bdbdaa8ff0ea1d56e922141722ab084c03f1ace2
filip <filip.ostojic@fer.hr>
2018-12-18 11:41:09 +0100

    new .sql file for new DB

--- Backend/autoserviceDB.sql
+++ Backend/autoserviceDB.sql
@@ -2,14 +2,15 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 9.6.2
--- Dumped by pg_dump version 9.6.2
+-- Dumped from database version 10.3
+-- Dumped by pg_dump version 10.3
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
 SET client_encoding = 'UTF8';
 SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
 SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
@@ -28,447 +29,344 @@ CREATE EXTENSION IF NOT EXISTS plpgsql WITH SCHEMA pg_catalog;
 COMMENT ON EXTENSION plpgsql IS 'PL/pgSQL procedural language';
 
 
-SET search_path = public, pg_catalog;
-
 SET default_tablespace = '';
 
 SET default_with_oids = false;
 
 --
--- Name: all_users; Type: TABLE; Schema: public; Owner: admin
+-- Name: appointment; Type: TABLE; Schema: public; Owner: admin
 --
 
-CREATE TABLE all_users (
-    email character varying(40) NOT NULL,
-    password character varying(100) NOT NULL,
-    role integer NOT NULL
+CREATE TABLE public.appointment (
+    id integer NOT NULL,
+    date timestamp without time zone NOT NULL,
+    description character varying(255),
+    repvehicle boolean NOT NULL,
+    mechanic_id character varying(255) NOT NULL,
+    service_id integer NOT NULL,
+    vehicle_id character varying(255) NOT NULL
 );
 
 
-ALTER TABLE all_users OWNER TO admin;
+ALTER TABLE public.appointment OWNER TO admin;
 
 --
--- Name: TABLE all_users; Type: COMMENT; Schema: public; Owner: admin
+-- Name: hibernate_sequence; Type: SEQUENCE; Schema: public; Owner: admin
 --
 
-COMMENT ON TABLE all_users IS 'svi korisnici';
+CREATE SEQUENCE public.hibernate_sequence
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
 
+
+ALTER TABLE public.hibernate_sequence OWNER TO admin;
 
 --
--- Name: mechanic; Type: TABLE; Schema: public; Owner: admin
+-- Name: model; Type: TABLE; Schema: public; Owner: admin
 --
 
-CREATE TABLE mechanic (
-    name character varying(20) NOT NULL,
-    surname character varying(20) NOT NULL,
-    email character varying(40) NOT NULL,
+CREATE TABLE public.model (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.model OWNER TO admin;
+
+--
+-- Name: role; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.role (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.role OWNER TO admin;
+
+--
+-- Name: service; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.service (
+    id integer NOT NULL,
+    name character varying(255) NOT NULL
+);
+
+
+ALTER TABLE public.service OWNER TO admin;
+
+--
+-- Name: servicevehicle; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.servicevehicle (
+    licenseplate character varying(255) NOT NULL,
+    rentedto character varying(255)
+);
+
+
+ALTER TABLE public.servicevehicle OWNER TO admin;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: admin
+--
+
+CREATE TABLE public.users (
+    email character varying(50) NOT NULL,
     mobile character varying(15) NOT NULL,
-    oib character varying(11) NOT NULL
-);
-
-
-ALTER TABLE mechanic OWNER TO admin;
-
---
--- Name: TABLE mechanic; Type: COMMENT; Schema: public; Owner: admin
---
-
-COMMENT ON TABLE mechanic IS 'serviser';
-
-
---
--- Name: models; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE models (
-    id integer NOT NULL,
-    name character varying(20) NOT NULL
-);
-
-
-ALTER TABLE models OWNER TO admin;
-
---
--- Name: TABLE models; Type: COMMENT; Schema: public; Owner: admin
---
-
-COMMENT ON TABLE models IS 'modeli automobila';
-
-
---
--- Name: roles; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE roles (
-    id integer NOT NULL,
-    name character varying(15) NOT NULL
-);
-
-
-ALTER TABLE roles OWNER TO admin;
-
---
--- Name: TABLE roles; Type: COMMENT; Schema: public; Owner: admin
---
-
-COMMENT ON TABLE roles IS 'uloge korisnika';
-
-
---
--- Name: services; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE services (
-    id integer NOT NULL,
-    service character varying(100) NOT NULL
-);
-
-
-ALTER TABLE services OWNER TO admin;
-
---
--- Name: TABLE services; Type: COMMENT; Schema: public; Owner: admin
---
-
-COMMENT ON TABLE services IS 'usluge servisa';
-
-
---
--- Name: timetable; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE timetable (
-    appointment timestamp without time zone NOT NULL,
-    mechanic character varying(11) NOT NULL,
-    licance_plate character varying(15) NOT NULL,
-    service integer NOT NULL,
-    description character varying(200),
-    rep_vehicle boolean NOT NULL
-);
-
-
-ALTER TABLE timetable OWNER TO admin;
-
---
--- Name: TABLE timetable; Type: COMMENT; Schema: public; Owner: admin
---
-
-COMMENT ON TABLE timetable IS 'raspored termina za prijavu automobila';
-
-
---
--- Name: user; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE "user" (
     name character varying(20) NOT NULL,
+    oib character varying(11),
+    password character varying(255) NOT NULL,
     surname character varying(20) NOT NULL,
-    email character varying(40) NOT NULL,
-    mobile character varying(20) NOT NULL,
-    oib character varying(11) NOT NULL
+    roleid integer
 );
 
 
-ALTER TABLE "user" OWNER TO admin;
+ALTER TABLE public.users OWNER TO admin;
 
 --
--- Name: TABLE "user"; Type: COMMENT; Schema: public; Owner: admin
+-- Name: uservehicle; Type: TABLE; Schema: public; Owner: admin
 --
 
-COMMENT ON TABLE "user" IS 'registrirani korisnik';
-
-
---
--- Name: vehicle_service; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE vehicle_service (
-    model integer NOT NULL,
-    licance_plate character varying(15) NOT NULL,
-    rented_to character varying(11)
+CREATE TABLE public.uservehicle (
+    licenseplate character varying(255) NOT NULL,
+    user_email character varying(255)
 );
 
 
-ALTER TABLE vehicle_service OWNER TO admin;
+ALTER TABLE public.uservehicle OWNER TO admin;
 
 --
--- Name: TABLE vehicle_service; Type: COMMENT; Schema: public; Owner: admin
+-- Name: vehicle; Type: TABLE; Schema: public; Owner: admin
 --
 
-COMMENT ON TABLE vehicle_service IS 'automobili servisa';
-
-
---
--- Name: vehicle_user; Type: TABLE; Schema: public; Owner: admin
---
-
-CREATE TABLE vehicle_user (
-    model integer NOT NULL,
-    licence_plate character varying(15) NOT NULL,
-    owner character varying(40) NOT NULL,
-    year character varying(4) NOT NULL
+CREATE TABLE public.vehicle (
+    licenseplate character varying(255) NOT NULL,
+    year character varying(255) NOT NULL,
+    modelid integer
 );
 
 
-ALTER TABLE vehicle_user OWNER TO admin;
+ALTER TABLE public.vehicle OWNER TO admin;
 
 --
--- Name: TABLE vehicle_user; Type: COMMENT; Schema: public; Owner: admin
+-- Data for Name: appointment; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COMMENT ON TABLE vehicle_user IS 'vozila registriranih korisnika';
-
-
---
--- Data for Name: all_users; Type: TABLE DATA; Schema: public; Owner: admin
---
-
-COPY all_users (email, password, role) FROM stdin;
+COPY public.appointment (id, date, description, repvehicle, mechanic_id, service_id, vehicle_id) FROM stdin;
 \.
 
 
 --
--- Data for Name: mechanic; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: model; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY mechanic (name, surname, email, mobile, oib) FROM stdin;
+COPY public.model (id, name) FROM stdin;
 \.
 
 
 --
--- Data for Name: models; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: role; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY models (id, name) FROM stdin;
+COPY public.role (id, name) FROM stdin;
 \.
 
 
 --
--- Data for Name: roles; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: service; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY roles (id, name) FROM stdin;
+COPY public.service (id, name) FROM stdin;
 \.
 
 
 --
--- Data for Name: services; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: servicevehicle; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY services (id, service) FROM stdin;
+COPY public.servicevehicle (licenseplate, rentedto) FROM stdin;
 \.
 
 
 --
--- Data for Name: timetable; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY timetable (appointment, mechanic, licance_plate, service, description, rep_vehicle) FROM stdin;
+COPY public.users (email, mobile, name, oib, password, surname, roleid) FROM stdin;
 \.
 
 
 --
--- Data for Name: user; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: uservehicle; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY "user" (name, surname, email, mobile, oib) FROM stdin;
+COPY public.uservehicle (licenseplate, user_email) FROM stdin;
 \.
 
 
 --
--- Data for Name: vehicle_service; Type: TABLE DATA; Schema: public; Owner: admin
+-- Data for Name: vehicle; Type: TABLE DATA; Schema: public; Owner: admin
 --
 
-COPY vehicle_service (model, licance_plate, rented_to) FROM stdin;
+COPY public.vehicle (licenseplate, year, modelid) FROM stdin;
 \.
 
 
 --
--- Data for Name: vehicle_user; Type: TABLE DATA; Schema: public; Owner: admin
+-- Name: hibernate_sequence; Type: SEQUENCE SET; Schema: public; Owner: admin
 --
 
-COPY vehicle_user (model, licence_plate, owner, year) FROM stdin;
-\.
-
-
---
--- Name: all_users all_users_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY all_users
-    ADD CONSTRAINT all_users_pkey PRIMARY KEY (email);
+SELECT pg_catalog.setval('public.hibernate_sequence', 1, false);
 
 
 --
--- Name: mechanic mechanic_oib_key; Type: CONSTRAINT; Schema: public; Owner: admin
+-- Name: appointment appointment_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY mechanic
-    ADD CONSTRAINT mechanic_oib_key UNIQUE (oib);
-
-
---
--- Name: mechanic mechanic_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY mechanic
-    ADD CONSTRAINT mechanic_pkey PRIMARY KEY (email);
+ALTER TABLE ONLY public.appointment
+    ADD CONSTRAINT appointment_pkey PRIMARY KEY (id);
 
 
 --
--- Name: models models_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+-- Name: model model_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY models
-    ADD CONSTRAINT models_pkey PRIMARY KEY (id);
-
-
---
--- Name: roles roles_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY roles
-    ADD CONSTRAINT roles_pkey PRIMARY KEY (id);
+ALTER TABLE ONLY public.model
+    ADD CONSTRAINT model_pkey PRIMARY KEY (id);
 
 
 --
--- Name: services services_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+-- Name: role role_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY services
-    ADD CONSTRAINT services_pkey PRIMARY KEY (id);
-
-
---
--- Name: timetable timetable_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY timetable
-    ADD CONSTRAINT timetable_pkey PRIMARY KEY (appointment, mechanic);
+ALTER TABLE ONLY public.role
+    ADD CONSTRAINT role_pkey PRIMARY KEY (id);
 
 
 --
--- Name: user user_oib_key; Type: CONSTRAINT; Schema: public; Owner: admin
+-- Name: service service_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_oib_key UNIQUE (oib);
-
-
---
--- Name: user user_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY "user"
-    ADD CONSTRAINT user_pkey PRIMARY KEY (email);
+ALTER TABLE ONLY public.service
+    ADD CONSTRAINT service_pkey PRIMARY KEY (id);
 
 
 --
--- Name: vehicle_service vehicle_service_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
+-- Name: servicevehicle servicevehicle_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY vehicle_service
-    ADD CONSTRAINT vehicle_service_pkey PRIMARY KEY (licance_plate);
-
-
---
--- Name: vehicle_user vehicle_user_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY vehicle_user
-    ADD CONSTRAINT vehicle_user_pkey PRIMARY KEY (licence_plate);
+ALTER TABLE ONLY public.servicevehicle
+    ADD CONSTRAINT servicevehicle_pkey PRIMARY KEY (licenseplate);
 
 
 --
--- Name: fki_timetable_licence_plate_fkey; Type: INDEX; Schema: public; Owner: admin
+-- Name: users uk_s8l2wh3rjdqc9r6j34c4ob3sw; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-CREATE INDEX fki_timetable_licence_plate_fkey ON timetable USING btree (licance_plate);
-
-
---
--- Name: fki_timetable_mechanic_fkey; Type: INDEX; Schema: public; Owner: admin
---
-
-CREATE INDEX fki_timetable_mechanic_fkey ON timetable USING btree (mechanic);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT uk_s8l2wh3rjdqc9r6j34c4ob3sw UNIQUE (oib);
 
 
 --
--- Name: fki_timetable_service_fkey; Type: INDEX; Schema: public; Owner: admin
+-- Name: users users_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-CREATE INDEX fki_timetable_service_fkey ON timetable USING btree (service);
-
-
---
--- Name: fki_vehicle_user_models_fkey; Type: INDEX; Schema: public; Owner: admin
---
-
-CREATE INDEX fki_vehicle_user_models_fkey ON vehicle_user USING btree (model);
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT users_pkey PRIMARY KEY (email);
 
 
 --
--- Name: all_users all_users_role_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- Name: uservehicle uservehicle_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY all_users
-    ADD CONSTRAINT all_users_role_fkey FOREIGN KEY (role) REFERENCES roles(id);
-
-
---
--- Name: timetable timetable_licence_plate_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY timetable
-    ADD CONSTRAINT timetable_licence_plate_fkey FOREIGN KEY (licance_plate) REFERENCES vehicle_user(licence_plate);
+ALTER TABLE ONLY public.uservehicle
+    ADD CONSTRAINT uservehicle_pkey PRIMARY KEY (licenseplate);
 
 
 --
--- Name: timetable timetable_mechanic_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- Name: vehicle vehicle_pkey; Type: CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY timetable
-    ADD CONSTRAINT timetable_mechanic_fkey FOREIGN KEY (mechanic) REFERENCES mechanic(oib);
-
-
---
--- Name: timetable timetable_service_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY timetable
-    ADD CONSTRAINT timetable_service_fkey FOREIGN KEY (service) REFERENCES services(id);
+ALTER TABLE ONLY public.vehicle
+    ADD CONSTRAINT vehicle_pkey PRIMARY KEY (licenseplate);
 
 
 --
--- Name: vehicle_service vehicle_service_model_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- Name: servicevehicle fk6sry69n892kq5kmfnkkssh85n; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY vehicle_service
-    ADD CONSTRAINT vehicle_service_model_fkey FOREIGN KEY (model) REFERENCES models(id);
-
-
---
--- Name: vehicle_service vehicle_service_rented_to_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
---
-
-ALTER TABLE ONLY vehicle_service
-    ADD CONSTRAINT vehicle_service_rented_to_fkey FOREIGN KEY (rented_to) REFERENCES "user"(oib);
+ALTER TABLE ONLY public.servicevehicle
+    ADD CONSTRAINT fk6sry69n892kq5kmfnkkssh85n FOREIGN KEY (licenseplate) REFERENCES public.vehicle(licenseplate);
 
 
 --
--- Name: vehicle_user vehicle_user_models_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- Name: appointment fk_mechanic_email; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY vehicle_user
-    ADD CONSTRAINT vehicle_user_models_fkey FOREIGN KEY (model) REFERENCES models(id);
+ALTER TABLE ONLY public.appointment
+    ADD CONSTRAINT fk_mechanic_email FOREIGN KEY (mechanic_id) REFERENCES public.users(email);
 
 
 --
--- Name: vehicle_user vehicle_user_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: admin
+-- Name: vehicle fk_model_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
 --
 
-ALTER TABLE ONLY vehicle_user
-    ADD CONSTRAINT vehicle_user_owner_fkey FOREIGN KEY (owner) REFERENCES "user"(oib);
+ALTER TABLE ONLY public.vehicle
+    ADD CONSTRAINT fk_model_id FOREIGN KEY (modelid) REFERENCES public.model(id);
+
+
+--
+-- Name: servicevehicle fk_reguser_email; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.servicevehicle
+    ADD CONSTRAINT fk_reguser_email FOREIGN KEY (rentedto) REFERENCES public.users(email);
+
+
+--
+-- Name: uservehicle fk_reguser_email; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.uservehicle
+    ADD CONSTRAINT fk_reguser_email FOREIGN KEY (user_email) REFERENCES public.users(email);
+
+
+--
+-- Name: users fk_role_id; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.users
+    ADD CONSTRAINT fk_role_id FOREIGN KEY (roleid) REFERENCES public.role(id);
+
+
+--
+-- Name: appointment fk_service_name; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.appointment
+    ADD CONSTRAINT fk_service_name FOREIGN KEY (service_id) REFERENCES public.service(id);
+
+
+--
+-- Name: appointment fk_uservehicle_licencplate; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.appointment
+    ADD CONSTRAINT fk_uservehicle_licencplate FOREIGN KEY (vehicle_id) REFERENCES public.uservehicle(licenseplate);
+
+
+--
+-- Name: uservehicle fkgrtj7770312h5edvsaexwoglq; Type: FK CONSTRAINT; Schema: public; Owner: admin
+--
+
+ALTER TABLE ONLY public.uservehicle
+    ADD CONSTRAINT fkgrtj7770312h5edvsaexwoglq FOREIGN KEY (licenseplate) REFERENCES public.vehicle(licenseplate);
 
 
 --
