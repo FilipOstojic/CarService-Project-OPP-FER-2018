0beefa588404bc3f5ea3c057233e2320e2cb59e8
FilipOstojic <filip.ostojic@fer.hr>
2018-12-19 11:48:23 +0100

    UserDAOImpl

---
+++ Backend/Autoservis/src/main/java/hr/fer/opp/dao/UserDAOImpl.java
@@ -0,0 +1,74 @@
+package hr.fer.opp.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import hr.fer.opp.model.User;
+
+@Component
+public class UserDAOImpl implements GenericDAO<User> {
+
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sessionFactory) {
+		this.sessionFactory = sessionFactory;
+	}
+
+	@Override
+	public void create(User user) {
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.persist(user);
+		tx.commit();
+		session.close();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<User> read() {
+		Session session = this.sessionFactory.openSession();
+		List<User> userList = session.createQuery("from User").list();
+		session.close();
+		return userList;
+	}
+
+	@SuppressWarnings("unchecked")
+	@Override
+	public User read(String email) {
+		Session session = this.sessionFactory.openSession();
+
+		@SuppressWarnings("rawtypes")
+		Query query = (Query) session.createQuery("from User where email =:atribute ");
+		query.setParameter("atribute", email);
+
+		List<User> userList = query.list();
+		session.close();
+		return userList.isEmpty() ? null : userList.get(0);
+	}
+
+	@Override
+	public void update(User user) {
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.merge(user);
+		tx.commit();
+		session.close();
+	}
+
+	@Override
+	public void delete(User user) {
+		Session session = this.sessionFactory.openSession();
+		Transaction tx = session.beginTransaction();
+		session.delete(user);
+		tx.commit();
+		session.close();
+	}
+
+}
