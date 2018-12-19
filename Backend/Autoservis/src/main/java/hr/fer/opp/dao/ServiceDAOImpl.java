85fc163b0934ed066ab5dec489156a510955127c
KarloFruhwirth <karlo.fruhwirth@fer.hr>
2018-12-19 11:42:06 +0100

    ServiceDAOImpl

--- Backend/Autoservis/src/main/java/hr/fer/opp/dao/ServiceDAOImpl.java
+++ Backend/Autoservis/src/main/java/hr/fer/opp/dao/ServiceDAOImpl.java
@@ -55,7 +55,7 @@ public class ServiceDAOImpl implements GenericDAO<Service> {
 		Transaction tx = session.beginTransaction();
 		session.merge(service);
 		tx.commit();
-		session.close();
+		session.close(); 
 	}
 
 	@Override
